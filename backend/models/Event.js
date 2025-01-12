3287ac11ea6e4adf4992a9d8704513af163431be
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-12 23:10:57 +0530

    first commit

---
+++ backend/models/Event.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const EventSchema  = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    images:{
+        type:[String],
+        default:[],
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    venue:{
+        type:String,
+        required:true,
+    },
+    city:{
+        type:String,
+        required:true,
+    },
+    totalseats:{
+        type:Number,
+        default:10,
+    },
+    minprice:{
+        type:Number,
+        default:0,
+    },
+    maxprice:{
+        type:Number,
+        default:100000000,
+    },
+    
+},{
+    timestamps:true,
+})
+module.exports = mongoose.model("Event",EventSchema)
