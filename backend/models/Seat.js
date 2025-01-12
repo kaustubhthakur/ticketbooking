67c2016291f362c00a794de86877e98b73076fb2
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-12 23:23:47 +0530

    first commit

--- backend/models/Seat.js
+++ backend/models/Seat.js
@@ -1,18 +1,18 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 const SeatSchema = new mongoose.Schema({
-  price:{ type:Number,default:0},
-   seatNumber:[
-    {
-       number:Number,
-       isbooked:{
-        type:Boolean,
-        default:false,
-       } 
-    }
-   ],
-   section:{
-    type:String,
-    default:""
-   },
+    price: { type: Number, default: 0 },
+    seatNumber: [
+        {
+            number: Number,
+            isbooked: {
+                type: Boolean,
+                default: false,
+            }
+        }
+    ],
+    section: {
+        type: String,
+        default: ""
+    },
 })
-module.exports = mongoose.model("Seat",SeatSchema)
+module.exports = mongoose.model("Seat", SeatSchema)
