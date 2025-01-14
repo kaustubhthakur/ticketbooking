4875cfea8142b63e6b3e160c87e1b35f3d380c7e
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-14 22:46:07 +0530

    first commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,32 +1,32 @@
 const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
+    username: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required:true,
+    email: {
+        type: String,
+        required: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
     },
-    bookings:{
-        type:[String],
-        default:[],
+    bookings: {
+        type: [String],
+        default: [],
     },
-    role:{
-        type:String,
-        enum:["user","creator"],
-        default:"user",
+    role: {
+        type: String,
+        enum: ["user", "organiser"],
+        default: "user",
     },
-profilePic:{
-    type:String,
-    default:"",
-}
+    profilePic: {
+        type: String,
+        default: "",
+    }
 },
-{
-    timestamps:String,
-})
-module.exports = mongoose.model("User",UserSchema);
+    {
+        timestamps: String,
+    })
+module.exports = mongoose.model("User", UserSchema);
