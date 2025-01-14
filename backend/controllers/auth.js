1b9aee057b6e2ce01e6f03a683b7dc8af707f6fc
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-14 22:40:18 +0530

    first commit

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,1 +1,23 @@
 const User = require('../models/User')
+const register = async(req,res)=> {
+    try {
+    
+    } catch (error) {
+        console.error(error);
+    }
+}
+const login = async(req,res)=> {
+    try {
+    
+    } catch (error) {
+        console.error(error);
+    }
+}
+const logout = async(req,res)=> {
+    try {
+    
+    } catch (error) {
+        console.error(error);
+    }
+}
+module.exports = {register,login,logout}
