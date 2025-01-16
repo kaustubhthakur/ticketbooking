0108cf9d4dbec566def8f09f73cd7f8285accdb1
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-16 22:07:24 +0530

    first commit users

---
+++ backend/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router();
+const {getUser,getUsers,deleteUser} = require('../controllers/users')
+const protectRoute = require('../utils/protectRoute')
+router.get('/:profile',protectRoute,getUser)
+router.get('/',getUsers)
+router.delete('/:id',protectRoute,deleteUser)
+module.exports = router;
