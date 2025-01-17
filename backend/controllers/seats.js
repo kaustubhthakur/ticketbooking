8e95c7cbd4897456a1f06c4cc8a2ca00c0ec8aed
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-17 22:13:25 +0530

    first commit

--- backend/controllers/seats.js
+++ backend/controllers/seats.js
@@ -1,11 +1,65 @@
 const Seat = require('../models/Seat')
 const Event = require('../models/Event')
 
-const createSeat = async(req,res)=> {
-try {
-    const eventid = req.params.id;
-} catch (error) {
-    console.error(error);
-}
-}
+const createSeat = async (req, res, next) => {
+    const eventId = req.params.eventid;
+    const newSeat = new Seat(req.body);
+
+    try {
+        const savedSeat = await newSeat.save();
+        try {
+            await Hotel.findByIdAndUpdate(eventIdId, {
+                $push: { seats: savedSeat._id },
+            });
+        } catch (err) {
+            next(err);
+        }
+        res.status(200).json(savedSeat);
+    } catch (err) {
+        next(err);
+    }
+};
+const updateSeat = async (req, res, next) => {
+    try {
+        const updatedSeat = await Seat.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        res.status(200).json(updatedSeat);
+    } catch (err) {
+        next(err);
+    }
+};
+const updateSeatAvailability = async (req, res, next) => {
+    try {
+        await Room.updateOne(
+            { "roomSeats._id": req.params.id },
+            {
+                $push: {
+                    "roomSeats.$.unavailableDates": req.body.dates
+                },
+            }
+        );
+        res.status(200).json("Room status has been updated.");
+    } catch (err) {
+        next(err);
+    }
+};
+export const deleteRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    try {
+        await Room.findByIdAndDelete(req.params.id);
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {
+                $pull: { rooms: req.params.id },
+            });
+        } catch (err) {
+            next(err);
+        }
+        res.status(200).json("Room has been deleted.");
+    } catch (err) {
+        next(err);
+    }
+};
 module.exports = {createSeat}
