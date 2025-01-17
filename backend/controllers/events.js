fb5d1d4082455ce533a8cfc94923f2c34a5384e5
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-17 22:05:04 +0530

    first commit

--- backend/controllers/events.js
+++ backend/controllers/events.js
@@ -8,4 +8,22 @@ const createEvent = async(req,res)=> {
         console.error(error);
     }
 }
-module.exports = {createEvent}
+const getEvents = async(req,res)=> {
+    try {
+        const events = await Event.find();
+        res.status(201).json(events);
+
+    } catch (error) {
+        console.error(error);
+
+    }
+}
+const getEvent = async(req,res)=>{
+    try {
+        const event = await Event.findById(req.params.id);
+        res.status(201).json(event);
+    } catch (error) {
+        console.error(error);
+    }
+}
+module.exports = {createEvent,getEvent,getEvents}
--- backend/controllers/seats.js
+++ backend/controllers/seats.js
@@ -2,6 +2,10 @@ const Seat = require('../models/Seat')
 const Event = require('../models/Event')
 
 const createSeat = async(req,res)=> {
-
+try {
+    const eventid = req.params.id;
+} catch (error) {
+    console.error(error);
+}
 }
 module.exports = {createSeat}
