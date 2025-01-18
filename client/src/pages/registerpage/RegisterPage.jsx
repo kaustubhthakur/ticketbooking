dd4bdb0a1c9e5ff40b68c746188f0f72897a48a3
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-18 23:11:08 +0530

    first commit

--- client/src/pages/registerpage/RegisterPage.jsx
+++ client/src/pages/registerpage/RegisterPage.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { UserDataContext } from '../../context/UserContext';
 
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userData, setUserData] = useState({});
 
-export default RegisterPage
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserDataContext);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const newUser = {
+      username,
+      email,
+      password,
+    };
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser);
+
+    if (response.status === 201) {
+      const data = response.data;
+      setUser(data.user);
+      localStorage.setItem('token', data.token);
+      navigate('/home');
+    }
+
+    setUsername('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <div className='p-7 h-screen flex flex-col justify-between'>
+        <div>
+          <img
+            className='w-16 mb-10'
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s'
+            alt=''
+          />
+
+          <form onSubmit={submitHandler}>
+            <h3 className='text-lg font-medium mb-2'>Choose a username</h3>
+            <input
+              required
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+              type='text'
+              placeholder='Username'
+            />
+
+            <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+            <input
+              required
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+              type='email'
+              placeholder='email@example.com'
+            />
+
+            <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+            <input
+              className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              required
+              type='password'
+              placeholder='password'
+            />
+
+            <button
+              className='bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
+            >
+              Create account
+            </button>
+          </form>
+          <p className='text-center'>
+            Already have an account? <Link to='/login' className='text-blue-600'>Login here</Link>
+          </p>
+        </div>
+        <div>
+          <p className='text-[10px] leading-tight'>
+            This site is protected by reCAPTCHA and the{' '}
+            <span className='underline'>Google Privacy Policy</span> and{' '}
+            <span className='underline'>Terms of Service apply</span>.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
