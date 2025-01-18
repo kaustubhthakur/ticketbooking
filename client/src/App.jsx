c7b2e4f1ee6910793816fc973da795ceef4b7fff
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-18 23:07:30 +0530

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import HomePage from './pages/homepage/HomePage'
+import RegisterPage from './pages/registerpage/RegisterPage'
+import LoginPage from './pages/loginpage/LoginPage'
+import Profile from './components/profile/Profile'
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<HomePage/>} />
+      <Route path='/register' element={<RegisterPage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route/>
     </Routes>
   )
 }
