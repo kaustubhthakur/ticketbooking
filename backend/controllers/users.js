8ac23d5fe297ba60f9fec377a3c1ef5f5bde58e3
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-24 22:53:17 +0530

    users commit

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,1 +1,18 @@
 const User = require('../models/User')
+const getUser = async(req,res)=>{
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+    }
+}
+const getUsers = async(req,res)=>{
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error);
+    }
+}
+module.exports = {getUser,getUsers}
