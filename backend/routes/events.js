a4bd6ec36ce62a6ded62deab78fc4c7367ee39b6
Kaustubh <kaustubhthakur76@gmail.com>
2025-02-12 23:03:37 +0530

    first commit

--- backend/routes/events.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const {createEvent,getEvent,getEvents} = require('../controllers/events')
-const protectRoute =require('../utils/protectRoute')
-const verifyAdmin = require('../utils/verifyAdmin')
-router.post('/',protectRoute,verifyAdmin,createEvent)
-router.get('/:id',protectRoute,verifyAdmin,getEvent)
-router.get('/',getEvents)
-module.exports = router;
